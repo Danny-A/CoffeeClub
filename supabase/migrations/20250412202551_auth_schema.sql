94ee7b8702278141933f7aa69da62e86768b49ad
Danny Arntz <arntz.D@gmail.com>
2025-04-14 20:57:27 +0200

    feat: add sql profile function

---
+++ supabase/migrations/20250412202551_auth_schema.sql
@@ -0,0 +1,18 @@
+-- Create the function first
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+RETURNS trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $function$
+begin
+  insert into public."Profile" (id, "avatarUrl")
+  values (new.id, 'https://www.gravatar.com/avatar/' || md5(new.email) || '?d=mp');
+  return new;
+end;
+$function$;
+
+-- Then create the trigger
+CREATE TRIGGER on_auth_user_created 
+AFTER INSERT ON auth.users 
+FOR EACH ROW 
+EXECUTE FUNCTION public.handle_new_user();

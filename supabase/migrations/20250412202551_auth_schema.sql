78177e5b9c72ba072d2703ff85c210b9820651a3
Danny Arntz <arntz.D@gmail.com>
2025-04-19 07:48:03 +0200

    fix: use correct fields for handle_new_user

--- supabase/migrations/20250412202551_auth_schema.sql
+++ supabase/migrations/20250412202551_auth_schema.sql
@@ -5,7 +5,7 @@ LANGUAGE plpgsql
 SECURITY DEFINER
 AS $function$
 begin
-  insert into public."Profile" (id, "avatarUrl")
+  insert into public."profiles" (id, "profile_image_url")
   values (new.id, 'https://www.gravatar.com/avatar/' || md5(new.email) || '?d=mp');
   return new;
 end;

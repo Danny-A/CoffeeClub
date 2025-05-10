36974a0f664e9e8767157204f4d6523dbac4e7f3
Danny Arntz <arntz.D@gmail.com>
2025-05-10 19:13:04 +0200

    fix: type on roaster

--- src/app/(main)/roasters/[id]/page.tsx
+++ src/app/(main)/roasters/[id]/page.tsx
@@ -152,7 +152,7 @@ export default async function RoasterPage({ params }: RoasterDetailsProps) {
                         rating: edge.node.rating,
                       })
                     ),
-                    averageRating: bean.node.average_rating,
+                    averageRating: bean.node.average_rating || undefined,
                   }}
                 />
               ))}

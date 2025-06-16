be7a48a317a76335ec984e01d7e2513e262d9405
Danny Arntz <arntz.D@gmail.com>
2025-06-16 13:02:11 +0200

    feat: initialize PostHog only in production environment

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,12 +1,13 @@
 import posthog from 'posthog-js';
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-  capture_pageview: 'history_change',
-  capture_pageleave: true, // Enable pageleave capture
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-  persistence: 'localStorage',
-  debug: process.env.NODE_ENV === 'development',
-  disable_session_recording: true,
-  loaded: (posthog) => {},
-});
+if (process.env.NODE_ENV === 'production') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    capture_pageview: 'history_change',
+    capture_pageleave: true, // Enable pageleave capture
+    capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+    persistence: 'localStorage',
+    disable_session_recording: true,
+    loaded: (posthog) => {},
+  });
+}
